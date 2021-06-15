3c1e408a47533dc8600b914cc98b5cd80e54a1a2
tinhh3 <tinhh3@fpt.com.vn>
2021-06-15 22:33:54 +0700

    fix: fs_storage

--- main/lib.py
+++ main/lib.py
@@ -63,7 +63,7 @@ def get_backup_status(backup_instances, latest_backup, latest_size):
             latest_backup = latest_backup + timedelta(hours=7)
 
         if latest_backup.date() >= now:
-            if latest_size * 100 / previous_backup_size <= 99:
+            if latest_size < previous_backup_size and latest_size * 100 / previous_backup_size <= 99:
                 return "WARNING (size less than yesterday backup size)"
             return "SUCCESS"
         else:
@@ -83,7 +83,7 @@ def get_latest_size(backup_instances, latest_backup):
             else:
                 latest_size = backup_instances.filter(date__exact=latest_backup)[0].size
                 for backup_instance in backup_instances:
-                    if backup_instance.date > latest_size:
+                    if backup_instance.size > latest_size:
                         latest_size = backup_instance.size
                 return latest_size
     except Exception as e:
