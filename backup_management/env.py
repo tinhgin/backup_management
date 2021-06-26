357ae57cfe5ba244f27ed498ba9a143879bec4ec
tinhh3 <tinhh3@fpt.com.vn>
2021-06-26 14:43:14 +0700

    update env

--- backup_management/env.py
+++ backup_management/env.py
@@ -8,7 +8,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = int(os.environ.get('DB_PORT'))
+DB_PORT = os.environ.get('DB_PORT')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
